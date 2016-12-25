ad899837991bb28bad6cab2fe2c9378ce760accd
master <yairyach@gmail.com>
2016-12-25 12:00:37 +0200

    AddedChat

---
+++ Client.py
@@ -0,0 +1,18 @@
+class Client():
+    def __init__(self, ip, port):
+        self._ip = ip
+        self._port = port
+        self._chatHistory = ""
+
+    def LoadChat(self):
+        # Load chat from file
+        pass
+
+    def SaveChat(self):
+        # Save chat to file
+        pass
+    def getSendAdd(self):
+        return (self._ip, int(self._port))
+
+
+
